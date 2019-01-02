2072d565e8aa55df1158bb0b1e943b9efeb805b1
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2019-01-02 08:18:56 -0600

    removes train file

--- dqn/run.sh
+++ dqn/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+OUTPUT_FILE=/u/prabhatn/repro_dqn/breakout1/stdout.txt
 ROM=/u/prabhatn/ale/thesis/dqn/roms/breakout.bin
 ARGS_OUTPUT_FILE=/u/prabhatn/repro_dqn/breakout1/args.txt
 EVAL_OUTPUT_FILE=/u/prabhatn/repro_dqn/breakout1/eval.txt
@@ -8,4 +9,4 @@ python dqn/main.py --rom $ROM \
 --args-output-file $ARGS_OUTPUT_FILE \
 --eval-output-file $EVAL_OUTPUT_FILE \
 --checkpoint-dir $CHECKPOINT_DIR \
---eval-init-states-file $EVAL_STATES_INIT_FILE
+--eval-init-states-file $EVAL_STATES_INIT_FILE &>> $STDOUT_FILE
--- dqn/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ROM=~/repro_dqn/dqn/roms/breakout.bin
-STDOUT_FILE=~/breakoutresults/stdout
-python ~/repro_dqn/dqn/main.py --rom $ROM &>> $STDOUT_FILE
